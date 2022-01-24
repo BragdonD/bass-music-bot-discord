4d52c0eaec12e359252b08fb94541c7b1ff43285
BragdonD <73294632+BragdonD@users.noreply.github.com>
2022-01-24 19:38:04 +0100

    play sound

--- src/index.mjs
+++ src/index.mjs
@@ -1,12 +1,8 @@
-import { Client, Intents, VoiceChannel } from "discord.js";
+import { Client, Intents } from "discord.js";
 import { AudioPlayer, AudioResource, createAudioPlayer, createAudioResource, joinVoiceChannel } from "@discordjs/voice";
-import fs, { createReadStream } from "fs";
-import ytdl from "ytdl-core";
+import { video_basic_info, stream } from 'play-dl';
+import youtubeSearch from "youtube-search";
 import dotenv from "dotenv";
-import ffmpeg from "ffmpeg-static"
-import path from "path";
-
-const __dirname = path.resolve(path.dirname(''));
 
 dotenv.config();
 
@@ -16,25 +12,62 @@ client.login( process.env.BOT_TOKEN );
 
 client.on("messageCreate", async (message) => {
     if(message.content.startsWith("!play")) {
+        const terms = message.content.split("!play").pop();
         const voiceChannel = message.member.voice.channel;
         if(!voiceChannel) {
             message.reply("You must be in a voice channel");
         }
         else {
-            const connection = joinVoiceChannel({
+            const opts = {
+                maxResults: 10,
+                key: process.env.YTB_SEARCH_API_KEY
+            };
+            
+            youtubeSearch(terms, opts, (err, results) => {
+                if(err) return console.log(err);
+              
+                //console.dir(results[0].thumbnails.);
+            });
+            message.channel.send()
+            /*const connection = joinVoiceChannel({
                 channelId: voiceChannel.id,
                 guildId: message.guild.id,
                 adapterCreator: message.guild.voiceAdapterCreator
             });
-            ytdl(`https://youtube.com/watch?v=n843kREfbdw`, {quality: "highestaudio"}).pipe(fs.createWriteStream('./data/video.mp4'));
-            let ressource = fs.createReadStream( path.join(__dirname,"./data/video.mp4") );
-            ressource.on("open", () => {
-                let player = createAudioPlayer(path.join(__dirname,"./data/video.mp4"));
-                player.play();
-            })
-            //console.log(path.join(__dirname,"./data/video.mp4"));
+            
+            const player = createAudioPlayer();
+
+            connection.subscribe(player);
+
+            const ytbRessource = await stream("https://youtube.com/watch?v=n843kREfbdw");
+
+            console.log(ytbRessource);
+
+            const audio = createAudioResource(ytbRessource.stream, {
+                inputType: ytbRessource.type
+            });
+
+            player.play(audio);*/
             
         }  
     }
 })
 
+/**
+ * 
+ * @param {String} term 
+ * @returns 
+ */
+const Search = (term) => {
+    try {
+        
+    } 
+    catch (e) {
+        console.log(e);
+        return null;
+    }
+}
+
+const handleBot = () => {
+
+}
